ebce59b9079ce702d1e04292c150feaa0f91a79b
Bluzzi <camilledugas.pro@gmail.com>
2021-07-07 12:09:54 +0200

    List of Pokémon TCG API endpoints

---
+++ src/lib/pokemontcg/Endpoints.ts
@@ -0,0 +1,12 @@
+// Documentation : https://pokemontcg.io/
+
+const api = "https://api.pokemontcg.io/v2/";
+
+export default {
+    cards: api + "cards",
+    sets: api + "sets",
+    types: api + "types",
+    subTypes: api + "subtypes",
+    superTypes: api + "supertypes",
+    rarities: api + "rarities"
+}
