12b28479bbc8c9ce11f6a896e59db30382c9f7a7
Bluzzi <camilledugas.pro@gmail.com>
2021-07-09 11:44:07 +0200

    Fetch (util): add typing

--- src/lib/utils/Fetch.ts
+++ src/lib/utils/Fetch.ts
@@ -1,9 +1,9 @@
-export async function jsonFetch(link, request = {}) : Promise<any> {
+export async function jsonFetch(link: string, request: RequestInit = {}) : Promise<any> {
     const response = await fetch(link, request);
 
     if(response.ok){
         return await response.json();
     } else {
-        throw new Error("Request failed");
+        throw new Error(link + " request failed");
     }
 }
