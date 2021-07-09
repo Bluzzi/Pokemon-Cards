7364ef68825b694843cee3f051f3749973bf2abd
Bluzzi <camilledugas.pro@gmail.com>
2021-07-09 13:27:51 +0200

    set/[id] (page): better loading system

--- src/routes/set/[id].svelte
+++ src/routes/set/[id].svelte
@@ -9,61 +9,88 @@
         }
     }
 </script>
+
+<script>
+    import Card from "$lib/components/elements/Card.svelte";
+    import { jsonFetch } from "$lib/utils/Fetch";
     import Endpoints from "$lib/pokemontcg/Endpoints";
+    import { currentSet, cards } from "$lib/stores/Store";
+    import { onMount } from "svelte";
 
-    export async function load({page, fetch, session, context}){
-        const setId = page.params.id;
+    // Get props :
+    export let setId: string;
 
-        const resSet = await fetch(Endpoints.sets + "/" + setId);
-        const resCard = await fetch(Endpoints.cards + "?q=set.id:" + setId);
+    // If the visitor has accessed this page without going through the navigation, 
+    // an additional request must be made to obtain the set information :
+    if(!$currentSet){
+        (async () => currentSet.set((await jsonFetch(Endpoints.sets + "/" + setId)).data))();
+    }
 
-        if(resSet.ok && resCard.ok){
-            const set = (await resSet.json()).data;
+    // Calls on every page load, not just the first one :
+    $: loadCards(), setId;
 
-            const data = await resCard.json();
-            const cards = data.data;
+    // Create a scroll event on the .cards div,
+    // because Svelte's on:scroll event does not work on an overflow-y div :
+    onMount(() => {
+        (document.querySelector(".cards") as HTMLDivElement).onscroll = scrollLoader;
+    });
 
-            if(data.totalCount > 250){
-                const resCard2 = await fetch(Endpoints.cards + "?page=2&q=set.id:" + setId);
+    // Functions :
+    const pageSize = 50;
 
-                if(resCard2.ok) cards.push(...(await resCard2.json()).data);
+    let totalCard: number;
+    let nextPage = {
+        end: false,
+        next: 1,
+        isLoaded: true
+    };
+
+    async function loadCards(page = 1){
+        jsonFetch(Endpoints.cards + "?page=" + page + "&pageSize=" + pageSize + "&q=set.id:" + setId)
+        .then(json => {
+            cards.update(cards => cards.concat(json.data));
+
+            totalCard = json.totalCount;
+            nextPage = {
+                end: $cards.length >= totalCard,
+                next: json.page + 1,
+                isLoaded: false
+            };
+        });
+    }
+
+    function scrollLoader(event: UIEvent){
+        const scrollDiv = <HTMLDivElement>event.target;
+
+        if(scrollDiv.scrollTop + window.innerHeight >= scrollDiv.scrollHeight){
+            if(!nextPage.end && !nextPage.isLoaded){
+                loadCards(nextPage.next);
+
+                nextPage.isLoaded = true;
             }
-
-            return {
-                props: {
-                    set: set,
-                    cards: cards
-                }
-            }
-        }
-        
-        return {
-            status: resCard.status,
-            error: new Error("Pokémon API don't work")
         }
     }
 </script>
 
-<script>
-    import type { ISet } from "$lib/pokemontcg/interfaces/Set";
-    import type { ICard } from "$lib/pokemontcg/interfaces/Card";
-    import Card from "$lib/components/elements/Card.svelte";
-
-    export let set: ISet;
-    export let cards: ICard[];
-</script>
-
 <svelte:head>
-    <title>Pokemon Cards - {set.series}</title>
+    <title>Pokemon Cards - {$currentSet?.series}</title>
 </svelte:head>
 
 <div class="serie">
-    <img src={set.images.logo} alt="serie logo" class="header">
+    {#if $currentSet?.images?.logo}
+        <img src={$currentSet.images.logo} alt="serie logo" class="header">
+    {/if}
 
     <div class="cards">
-        {#each cards as card}
+        {#each $cards as card}
             <Card card={card}/>
-        {/each}    
+        {/each}
+
+        {#if $cards.length < totalCard}
+            <div class="load">
+                <img src="/img/loading.svg" alt="loading">    
+            </div>
+        {/if}
     </div>
 </div>
 
@@ -76,8 +103,7 @@
         .header {
             margin: 40px 0;
 
-            height: max-content;
-            width: 50%;
+            width: 450px;
 
             @media (max-width: $responsive-bp-tablet) {
                 width: 95%;
@@ -92,6 +118,14 @@
             flex-wrap: wrap;
 
             margin-bottom: 40px;
+        }
+
+        .load {
+            width: 100%;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
         }
     }
 </style>
