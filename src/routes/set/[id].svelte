56f5079266fe816d76b1479455db506bf4a14f74
Bluzzi <camilledugas.pro@gmail.com>
2021-07-11 21:38:57 +0200

    set/[id]: clean code and fix bugs

--- src/routes/set/[id].svelte
+++ src/routes/set/[id].svelte
@@ -16,14 +16,27 @@
     import { jsonFetch } from "$lib/utils/Fetch";
     import Endpoints from "$lib/pokemontcg/Endpoints";
     import { currentSet } from "$lib/stores/Store";
-    import { afterUpdate, onMount } from "svelte";
     import type { ICard } from "$lib/pokemontcg/interfaces/Card";
+
+    interface INextPage {
+        end: boolean,
+        next: number,
+        isLoaded: boolean
+    }
 
     // Get props :
     export let setId: string;
     export let cardsList: ICard[];
 
+    // Bind div variables :
+    let serieDiv: HTMLDivElement;
     let cardsDiv: HTMLDivElement;
+
+    // Card loading configuration :
+    const pageSize = 50;
+
+    let totalCard: number;
+    let nextPage: INextPage;
 
     // If the visitor has accessed this page without going through the navigation, 
     // an additional request must be made to obtain the set information :
@@ -32,29 +45,24 @@
     }
 
     // Calls on every page load, not just the first one :
-    $: loadCards(), setId;
-
-    // Create a scroll event on the .cards div,
-    // because Svelte's on:scroll event does not work on an overflow-y div :
-    onMount(() => {
-        (document.querySelector(".cards") as HTMLDivElement).onscroll = scrollLoader;
-    });
-
-    afterUpdate(() => {
-        if(!cardsList.length && $currentSet && setId === $currentSet.id){
-            cardsDiv.scrollTo(0, 0);
-        }
-    });
-
-    // Functions :
-    const pageSize = 50;
-
-    let totalCard: number;
-    let nextPage = {
+    $: nextPage = {
         end: false,
         next: 1,
         isLoaded: true
-    };
+    }, setId;
+    $: reloadPage(), setId;
+
+    // Functions :
+    async function reloadPage(){
+        // Remove old cards :
+        if(cardsDiv) cardsDiv.innerHTML = "";
+
+        // Scroll to the page top :
+        if(serieDiv) serieDiv.scrollTo(0, 0);
+
+        // Load new cards :
+        loadCards();
+    }
 
     async function loadCards(page = 1){
         jsonFetch<any>(Endpoints.cards + "?page=" + page + "&pageSize=" + pageSize + "&q=set.id:" + setId)
@@ -62,15 +70,14 @@
             cardsList = cardsList.concat(json.data);
 
             totalCard = json.totalCount;
-            nextPage = {
-                end: cardsList.length >= totalCard,
-                next: json.page + 1,
-                isLoaded: false
-            };
+
+            nextPage.end = cardsList.length >= totalCard;
+            nextPage.next = json.page + 1;
+            nextPage.isLoaded = false;
         });
     }
 
-    function scrollLoader(event: UIEvent){
+    async function scrollLoader(event: UIEvent){
         const scrollDiv = <HTMLDivElement>event.target;
 
         if(scrollDiv.scrollTop + window.innerHeight >= scrollDiv.scrollHeight){
@@ -87,8 +94,8 @@
     <title>Pokemon Cards - {$currentSet?.series}</title>
 </svelte:head>
 
-<div class="serie">
-    <p id="tgm">{cardsList.length}</p>
+<div class="serie" on:scroll={scrollLoader} bind:this={serieDiv}>
+    <p id="tgm">{cardsList.length}/{totalCard}</p>
 
     {#if $currentSet}
         <img src={$currentSet.images.logo} alt="serie logo" class="header">
@@ -112,6 +119,10 @@
         display: flex;
         align-items: center;
         flex-direction: column;
+
+        width: 100%;
+
+        overflow-y: scroll;
 
         #tgm {
             position: fixed;
