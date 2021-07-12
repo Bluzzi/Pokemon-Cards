9536490c7efae587dabd4705e9cf5725796a309d
Bluzzi <camilledugas.pro@gmail.com>
2021-07-12 12:49:17 +0200

    NavSeries: add burger menu for mobile device

--- src/lib/components/navigation/navseries/NavSeries.svelte
+++ src/lib/components/navigation/navseries/NavSeries.svelte
@@ -1,7 +1,8 @@
 <script>
     import type { ISet } from "$lib/pokemontcg/interfaces/Set";
     import SerieSets from "$lib/components/navigation/navseries/SerieSets.svelte";
-    import { onMount } from "svelte";
+    import { onDestroy, onMount } from "svelte";
+    import { navigating } from "$app/stores";
 
     export let sets: ISet[];
 
@@ -32,22 +33,118 @@
             openSerieName = series[0];
         }
     });
+
+    // Functions :
+    let burgerOpen = false;
+
+    function changeMenuState(state: boolean|null = null){
+        burgerOpen = typeof state === "boolean" ? state : !burgerOpen;
+    }
+
+    // Change menu state on navigating :
+    const unSubscriber = navigating.subscribe(() => {
+        changeMenuState(false);
+    });
+
+    onDestroy(unSubscriber);
 </script>
 
-<div class="nav-sets">
+<div class="burger" on:click={() => changeMenuState()} class:burger-active={burgerOpen}>
+    <span></span>
+    <span></span>
+    <span></span>
+</div>
+
+<div class="nav-sets" class:nav-active={burgerOpen} class:nav-inactive={!burgerOpen}>
     {#each series as serie}
         <SerieSets serie={serie} sets={setsBySeries[serie]} isOpen={serie === openSerieName}/>
     {/each}
 </div>
 
 <style>
+    // Burger :
+    $bar-margin: 8px;
+
+    $bar-height: 5px;
+    $bar-width: ($bar-margin * 2) + ($bar-height * 3);
+
+    .burger {
+        height: $bar-width;
+        width: $bar-width;
+
+        margin: 10px;
+        padding: 10px;
+
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-direction: column;
+
+        position: absolute;
+        right: 0;
+
+        z-index: $z-burger;
+
+        background-color: $color-primary;
+        
+        border-radius: 5px;
+
+        cursor: pointer;
+
+        span {
+            height: $bar-height;
+            width: $bar-width;
+
+            background-color: $color-yellow;
+
+            border-radius: 2px;
+
+            transition: all 0.5s;
+        }
+
+        @media (min-width: $responsive-bp-mobile) {
+            display: none;
+        }
+    }
+
+    .burger-active span {
+        &:nth-child(1) {
+            transform: translateY($bar-margin + $bar-height) rotate(45deg);
+        }
+
+        &:nth-child(2) {
+            opacity: 0;
+        }
+
+        &:nth-child(3) {
+            transform: translateY(-($bar-margin + $bar-height)) rotate(-45deg);
+        }
+    }
+
+    // Nav :
     .nav-sets {
         width: max-content;
 
         background-color: $color-white;
 
         overflow-y: scroll;
+    }
 
-        margin: 0;
+    .nav-active {
+        position: fixed;
+
+        top: $navbar-height;
+        bottom: $footer-line-height;
+
+        height: calc(100% - #{$navbar-height} - #{$footer-line-height});
+        width: 100vw;
+
+        z-index: $z-serie-menu;
+    }
+
+    .nav-inactive {
+        @media (max-width: $responsive-bp-mobile) {
+            display: none;
+        }
     }
 </style>
