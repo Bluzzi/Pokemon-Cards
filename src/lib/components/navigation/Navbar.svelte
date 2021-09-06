95bd4475d377045fba597f0c7e4be06997a85144
Bluzzi <camilledugas.pro@gmail.com>
2021-09-06 15:08:55 +0200

    Navbar (component): add search input

--- src/lib/components/navigation/Navbar.svelte
+++ src/lib/components/navigation/Navbar.svelte
@@ -18,10 +18,23 @@
     });
 
     onDestroy(unSubscribe);
+
+    // Functions :
+    function onSearch(event: KeyboardEvent){
+        if(event.key === "Enter"){
+            console.log("SEARCH !!");
+
+            return;
+        }
+
+        console.log((event.target as HTMLInputElement).value); // TODO
+    }
 </script>
 
 <nav>
     <h1>Pokemon Cards</h1>
+
+    <input type="text" placeholder="Search a Pokemon 🔍" on:keyup={onSearch} id="search-bar">
 
     <div class="links">
         {#each navItems as item}
@@ -47,6 +60,21 @@
             font-size: 1.3em;
 
             margin-right: 20px;
+        }
+
+        input {
+            height: 55%;
+            width: 50%;
+
+            padding: 0 15px;
+
+            border: none;
+            border-radius: 3px;
+            outline: none;
+
+            &:focus {
+                border: solid 3px $color-yellow;
+            }
         }
 
         a {
